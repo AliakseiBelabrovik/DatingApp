953c0cf51d0c1394a45a55ce4942f471b739df58
Aliaksei <aliaksei.belabrovik@pwc.com>
2023-12-13 22:10:56 +0100

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //means, the route is api + / + take the first word from the class name
+// => /api/users
+public class UsersController : ControllerBase
+{
+    private DataContext Context { get; }
+
+    public UsersController(DataContext context)
+    {
+        Context = context;
+    }
+
+    //Task is a asynchronous result
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await Context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")] //api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await this.Context.Users.FindAsync(id);
+    }
+}
+
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    //should be called Id so that Entity Framework knows what is a foreign key
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+}
